2f019e6a4d22dd7b75caa881f3b4be4fd5705184
Nikita Olejnik <nikitinoleg374@gmail.com>
2017-11-09 11:59:35 +0200

    File

---
+++ src/Weight/Weight.java
@@ -0,0 +1,10 @@
+package Weight;
+
+public class Weight {
+    public static void main(String[] args) {
+        double m=92.2;
+        double mmoon = m*0.17;
+        System.out.println(m+ " вес на Земле " + mmoon+ " вес на Луне ");
+
+    }
+}
